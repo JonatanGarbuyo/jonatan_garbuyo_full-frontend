006c7b79346aaea1a4285719c67c910ab54da1e8
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-06-11 10:57:15 -0300

    refactor Header component

---
+++ components/header.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+
+import logo from '/public/images/logo.svg'
+import logoSmall from '/public/images/logoSmall.svg'
+import light from '/public/icon/light.svg'
+import logout from '/public/icon/logout.svg'
+
+import styles from './header.module.css'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={`hidden-xs ${styles.logo__image__wrapper}`}>
+        <Image src={logo} alt="logo" layout={'fill'} />
+      </div>
+      <div className={`${styles.logo__image__wrapper} hidden-sm`}>
+        <Image src={logoSmall} alt="logo" layout={'fill'} />
+      </div>
+      <nav>
+        <ul>
+          <li className={styles.lemon}>Buscar</li>
+          <li>Mis Albumes</li>
+          <li>
+            <div />
+          </li>
+          <li>
+            <span className={`hidden-xs`}>Cerrar sesión</span>
+            <span id="logout" className={`${styles.logout} visible-xs-block`}>
+              <Image src={logout} alt="logout" layout="fill" />
+            </span>
+          </li>
+
+          <li className={`hidden-sm`}>
+            <div />
+          </li>
+          <li className={`${styles.light}  visible-xs-block  hidden-sm`}>
+            <Image src={light} alt="light icon" layout="fill" />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import Header from '/components/header'
 import test from '/public/images/test.png'
-import logo from '/public/images/logo.svg'
-import logoSmall from '/public/images/logoSmall.svg'
-import light from '/public/icon/light.svg'
-import logout from '/public/icon/logout.svg'
 import styles from '/styles/Home.module.css'
 
 export default function Home() {
@@ -48,37 +45,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-
-      <header className={styles.header}>
-        <div className={`hidden-xs ${styles.logo__image__wrapper}`}>
-          <Image src={logo} alt="logo" layout={'fill'} />
-        </div>
-        <div className={`${styles.logo__image__wrapper} hidden-sm`}>
-          <Image src={logoSmall} alt="logo" layout={'fill'} />
-        </div>
-        <nav>
-          <ul>
-            <li className={styles.lemon}>Buscar</li>
-            <li>Mis Albumes</li>
-            <li>
-              <div />
-            </li>
-            <li>
-              <span className={`hidden-xs`}>Cerrar sesión</span>
-              <span id="logout" className={`${styles.logout} visible-xs-block`}>
-                <Image src={logout} alt="logout" layout="fill" />
-              </span>
-            </li>
-
-            <li className={`hidden-sm`}>
-              <div />
-            </li>
-            <li className={`${styles.light}  visible-xs-block  hidden-sm`}>
-              <Image src={light} alt="light icon" layout="fill" />
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
 
       <main className={styles.main}>
         <div className={styles.search__title}>
