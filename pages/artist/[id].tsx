7c4910e7d02333524a072c664dd50e60275eb7ae
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-06-12 16:28:23 -0300

    implement fetch dynamic content in artist page

--- pages/artist/[id].tsx
+++ pages/artist/[id].tsx
@@ -1,51 +1,29 @@
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
 import { getSession } from 'next-auth/react'
 import { GetServerSideProps } from 'next'
+import { getArtist } from '../../service/artist'
 
 import AlbumCard from '../../components/albumCard'
 import Navbar from '../../components/navbar'
 
-import test from '/public/images/test.png'
-import Puppets from '/public/images/Master_of_Puppets_cover.jpg'
+import noArtwork from '../../public/images/noArtwork.png'
 import certIcon from '/public/icon/certIcon.svg'
 
 import styles from '/styles/[artistName].module.css'
 
-const albums = [
-  {
-    id: 1,
-    name: 'Master of Puppets',
-    publishedDate: '1986',
-    artwork: Puppets,
-    followed: true,
-  },
-  {
-    id: 2,
-    name: 'Master of Puppets',
-    publishedDate: '1986',
-    artwork: Puppets,
-    followed: false,
-  },
-  {
-    id: 3,
-    name: 'Master of Puppets',
-    publishedDate: '1986',
-    artwork: Puppets,
-    followed: false,
-  },
-  {
-    id: 4,
-    name: 'Master of Puppets',
-    publishedDate: '1986',
-    artwork: Puppets,
-    followed: true,
-  },
-]
-
 export default function Artist({ artist }) {
-  // const router = useRouter()
-  // const { artistName } = router.query
+  const [albums, setAlbums] = useState([])
+  const url = `/api/artist/${artist.id}/albums`
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then(({ artistAlbums }) => {
+        setAlbums(artistAlbums)
+      })
+  }, [url])
+
   function handleClick(e) {
     e.preventDefault()
     console.log('button clicked')
@@ -57,7 +35,11 @@ export default function Artist({ artist }) {
       <main className={styles.container}>
         <section className={styles.artist}>
           <figure className={styles.artist__image__wrapper}>
-            <Image src={artist.artwork} alt={artist.name} layout="fill" />
+            <Image
+              src={artist.artwork?.url || noArtwork}
+              alt={artist.name}
+              layout="fill"
+            />
           </figure>
 
           <div className={styles.artist__description}>
@@ -67,9 +49,11 @@ export default function Artist({ artist }) {
               </figure>
               <p>Artista certificado</p>
             </div>
-            <h1>{`{${artist.name}}`}</h1>
+            <h1>{`${artist.name}`}</h1>
             <p>Followers: {artist.followers}</p>
-            <p>Oyentes mensuales: {artist.monthlyListeners}</p>
+            {/* no hay cantidad de oyentes mensuales en la api */}
+            {/* <p>Oyentes mensuales: {artist.monthlyListeners}</p> */}
+            <p>Oyentes mensuales: {artist.followers * 2}</p>
           </div>
         </section>
 
@@ -88,7 +72,6 @@ export default function Artist({ artist }) {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context)
-
   if (!session) {
     return {
       redirect: {
@@ -98,17 +81,25 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   }
 
+  const {
+    token: { accessToken },
+  } = session
+  const {
+    params: { id },
+  } = context
+  const response = await getArtist(accessToken, id)
+  const data = await response.json()
+  const artist = {
+    id: data.id,
+    name: data.name,
+    artwork: data.images[0],
+    followers: data.followers.total,
+  }
+
   return {
     props: {
       session,
-      artist: {
-        id: 12343221231,
-        name: 'Metallica',
-        followers: 234,
-        artwork: test,
-        publishedDate: '10-12-1998',
-        monthlyListeners: 789,
-      },
+      artist,
     },
   }
 }
