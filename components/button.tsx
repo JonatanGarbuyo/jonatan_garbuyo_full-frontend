f5fbf961612a09c5935d2c63b35e61550f787e50
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-06-12 13:30:26 -0300

    add default values

--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './button.module.css'
 
-export default function Button({ children, className, onClick }) {
+export default function Button({ children, className, onClick = null }) {
   return (
     <button className={`${styles.button} ${className}`} onClick={onClick}>
       {children}
