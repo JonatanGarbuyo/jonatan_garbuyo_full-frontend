dd5253853ade879da98a2b279b4b8be9559b0614
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-06-12 13:29:28 -0300

    add search artist api

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,22 @@ import SpotifyProvider from 'next-auth/providers/spotify'
 export default NextAuth({
   providers: [
     SpotifyProvider({
+      scope:
+        'user-read-email playlist-modify-public playlist-modify-private user-library-modify user-library-read',
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.refresh_token
+      }
+      return token
+    },
+    async session(session, user) {
+      session.user = user
+      return session
+    },
+  },
 })
